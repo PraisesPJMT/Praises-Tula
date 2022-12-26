65d70d2be619e13d47ddac49f82c1b9f360a9d8a
PraisesPJMT <praisesmusa@outlook.com>
2022-12-26 23:16:20 +0100

    Add nav links and routes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Link, NavLink,
+} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faClose } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
+import ContactLink from '../footer/ContactLink';
 
-const Header = () => (
-  <header>
-    <a className="logo" href="/">
-      Praises-Tula
-    </a>
-    <button type="button" className="mobile-menu-control">
-      <FontAwesomeIcon className="bars" icon={faBars} />
-      <FontAwesomeIcon className="close" icon={faClose} />
-    </button>
-  </header>
-);
+const Header = () => {
+  const [mobileMenu, setMobileMenu] = useState(true);
+  const NavBars = [
+    { path: '/', link: '_hello' },
+    { path: '/', link: '_about-me' },
+    { path: '/', link: '_projects' },
+    { path: '/', link: '_contact-me' },
+  ];
+
+  const handleMenu = () => {
+    const header = document.querySelector('header');
+    const nav = document.querySelector('.nav');
+    const bars = document.querySelector('.bars');
+    const close = document.querySelector('.close');
+    if (mobileMenu) {
+      header.classList.remove('mobile-opened');
+      header.classList.add('mobile-closed');
+      nav.classList.add('hide');
+      close.classList.add('hide');
+      bars.classList.remove('hide');
+    } else {
+      header.classList.remove('mobile-closed');
+      header.classList.add('mobile-opened');
+      nav.classList.remove('hide');
+      close.classList.remove('hide');
+      bars.classList.add('hide');
+    }
+    setMobileMenu(!mobileMenu);
+  };
+  return (
+    <header className="header">
+      <div className="mobile-container">
+        <Link className="logo" to="/">
+          praises-tula
+        </Link>
+        <button
+          type="button"
+          className="mobile-menu-control"
+          onClick={handleMenu}
+          title="mobile menu button"
+        >
+          <FontAwesomeIcon className="bars" icon={faBars} />
+          <FontAwesomeIcon className="close hide" icon={faClose} />
+        </button>
+      </div>
+
+      <nav className="nav hide">
+        {
+          NavBars.map((nav) => (
+            <NavLink
+              to={nav.path}
+              key={nav.link}
+              onClick={handleMenu}
+            >
+              { nav.link }
+            </NavLink>
+          ))
+        }
+      </nav>
+      { mobileMenu ? (<ContactLink />) : '' }
+    </header>
+  );
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Header from './components/header/Header';
 
 const AppRoot = ReactDOM.createRoot(document.getElementById('root'));
 AppRoot.render(
   <React.StrictMode>
-    <Header />
     <App />
   </React.StrictMode>,
 );
