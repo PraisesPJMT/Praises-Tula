b12d41ce166c8689e542f73e479a1f23a1d2ce8b
PraisesPJMT <praisesmusa@outlook.com>
2023-01-15 10:29:01 +0100

    Add `ProjectList` component

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ProjectCard from './ProjectCard';
+// eslint-disable-next-line import/no-cycle
+import Projects from '../../assets/data';
+
+const ProjectList = () => (
+  <div className="project-list">
+    <h3 className="sub-heading2">
+      <span className="harsh">## </span>
+      projects
+      <span className="text">
+        <span className="mono"> &frasl; </span>
+        all
+      </span>
+    </h3>
+    <div className="projects-container">
+      { Projects.map((project) => (
+        <ProjectCard key={project.id} project={project} />
+      ))}
+    </div>
+  </div>
+);
+
+export default ProjectList;
